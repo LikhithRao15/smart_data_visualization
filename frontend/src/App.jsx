8e8db8a5601282635d54adda78835067872a5f69
Likhith Rao K <likhithraok@Likhiths-MacBook-Air.local>
2025-11-25 05:53:46 +0530

    api_change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -81,7 +81,7 @@ export default function App() {
     formData.append("file", fileObj);
 
     try {
-      const res = await axios.post(`https://smart-data-visualization.onrender.com/analyze`, formData, {
+      const res = await axios.post("/api/analyze", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       console.log("BACKEND RESPONSE:", res.data);
